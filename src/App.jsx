42e1318ae0b1fd272661755740ee28a74e82d0fc
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-03-13 22:46:09 +0200

    implement main pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from './pages/Product';
 import Pricing from "./pages/Pricing";
-import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import AppLayout from "./pages/AppLayout";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<NotFound />} />
 
