0b46924670807f39a34306ec9e5691d87aa5e2a4
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-05-07 16:17:53 +0300

    improve ui elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import NotFound from "./pages/NotFound";
@@ -8,30 +8,32 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
-/* eslint-ignore */ 
-const BASE_URL = "http://localhost:8000"
+import City from "./components/City";
+import Form from "./components/Form";
+/* eslint-ignore */
+const BASE_URL = "http://localhost:8000";
 export default function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(()=> {     async function fetchCities() {
+  useEffect(() => {
+    async function fetchCities() {
       try {
-        setIsLoading(true)
+        setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
-        if(res.ok) {
+        if (res.ok) {
           const data = await res.json();
           console.log(data);
-          setCities(data)
+          setCities(data);
         }
       } catch (error) {
         console.error("error in fetchCities()", error);
-        
-      }finally {
-        setIsLoading(false)
+      } finally {
+        setIsLoading(false);
       }
     }
     fetchCities();
-  } ,[])
+  }, []);
 
   return (
     <BrowserRouter>
@@ -41,10 +43,18 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path="form" element={<p>Form</p>} />
+          {/* <Route index element={<CityList cities={cities} isLoading={isLoading} />} /> */}
+          <Route index element={<Navigate replace to="cities" />} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './CityItem.module.css'
+import { Link } from 'react-router-dom';
 
 
 const formatDate = (date) =>
@@ -9,15 +10,17 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 export default function CityItem({city}) {
-    const {cityName, emoji, date} = city
-    console.log(emoji);
+    const {cityName, emoji, date, id, position} = city
+    console.log(position);
     
   return (
-    <li className={styles.cityItem}>
+    <li >
+      <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}> {cityName} </h3>
       <time className={styles.date}> ({formatDate(date)}) </time>
       <button className={styles.deleteBtn}>&times;</button>
+    </Link>
     </li>
   )
 }
