d1a7f92bd98d8753ad6680c47563f05d023175f3
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-05-12 16:11:41 +0300

    implement delete city fuctionality

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
 import { useCitiesContext } from "../contexts/CitiesContext";
+import Spinner from "./Spinner";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,8 +12,16 @@ const formatDate = (date) =>
   }).format(new Date(date));
 export default function CityItem({ city }) {
   const { currentCity } = useCitiesContext();
+  const {isLoading, deleteCity} = useCitiesContext();
   const { cityName, emoji, date, id, position } = city;
+  function handleDeleteCity(e) {
+    e.preventDefault();
+    deleteCity(city.id);
+  }
 
+  if(isLoading) {
+    return <Spinner />
+  }
 
   return (
     <li>
@@ -25,7 +34,7 @@ export default function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}> {cityName} </h3>
         <time className={styles.date}> ({formatDate(date)}) </time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleDeleteCity}>&times;</button>
       </Link>
     </li>
   );
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -9,35 +9,36 @@ export default function CitiesContextProvider({ children }) {
   const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        if (res.ok) {
-          const data = await res.json();
-          setCities(data);
-        }
-      } catch (error) {
-        console.error("error in fetchCities()", error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
+    
     fetchCities();
   }, []);
+  async function fetchCities() {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`);
+      if (res.ok) {
+        const data = await res.json();
+        setCities(data);
+      }
+    } catch (error) {
+      console.error("error in fetchCities()", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
   async function getCurrentCity(id) {
     try {
       setIsLoading(true);
-      console.log("before if");
+      // console.log("before if");
       
       const res = await fetch(`${BASE_URL}/cities/${id}`);
-      console.log("after fetch");
+      // console.log("after fetch");
       
       if (res.ok) {
-        console.log("res.OK");
+        // console.log("res.OK");
         const data = await res.json();
         setCurrentCity(data);
-        console.log("current city: ", data);
+        // console.log("current city: ", data);
       }
     } catch (error) {
       console.error("error in fetchCities()", error);
@@ -68,6 +69,26 @@ export default function CitiesContextProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE"
+      })
+
+      if(res.ok) {
+        console.log("city removed successfully");
+        fetchCities()
+        
+      }
+      
+    } catch (error) {
+      console.error("error id deleteCity()", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
@@ -75,7 +96,8 @@ export default function CitiesContextProvider({ children }) {
         isLoading,
         getCurrentCity,
         currentCity,
-        createNewCity
+        createNewCity,
+        deleteCity
       }}
     >
       {children}
